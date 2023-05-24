f69192916f7e635f8b9f3b5cf1e317eed7260eb9
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-24 03:29:05 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,13 +5,21 @@ import sys
 
 
 solutions = []
+"""The list of possible solutions to the N queens problem.
+"""
 n = 0
+"""The size of the chessboard.
+"""
 pos = None
-
+"""The list of possible positions on the chessboard.
+"""
 
 
 def get_input():
-    """
+    """Retrieves and validates this program's argument.
+
+    Returns:
+        int: The size of the chessboard.
     """
     global n
     n = 0
@@ -30,7 +38,14 @@ def get_input():
 
 
 def is_attacking(pos0, pos1):
-    """
+    """Checks if the positions of two queens are in an attacking mode.
+
+    Args:
+        pos0 (list or tuple): The first queen's position.
+        pos1 (list or tuple): The second queen's position.
+
+    Returns:
+        bool: True if the queens are in an attacking position else False.
     """
     if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
         return True
@@ -38,7 +53,13 @@ def is_attacking(pos0, pos1):
 
 
 def group_exists(group):
-    """
+    """Checks if a group exists in the list of solutions.
+
+    Args:
+        group (list of integers): A group of possible positions.
+
+    Returns:
+        bool: True if it exists, otherwise False.
     """
     global solutions
     for stn in solutions:
@@ -53,7 +74,11 @@ def group_exists(group):
 
 
 def build_solution(row, group):
-    """
+    """Builds a solution for the n queens problem.
+
+    Args:
+        row (int): The current row in the chessboard.
+        group (list of lists of integers): The group of valid positions.
     """
     global solutions
     global n
@@ -73,7 +98,7 @@ def build_solution(row, group):
 
 
 def get_solutions():
-    """
+    """Gets the solutions for the given chessboard size.
     """
     global pos, n
     pos = list(map(lambda x: [x // n, x % n], range(n ** 2)))
